9bdfdaec51e6a5a5bcad1edd7381a68fe7559930
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-09-20 23:39:20 +0200

    feat: orthographic camera
    Added also to the parser.

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -4,6 +4,7 @@ mod mesh;
 pub use self::mesh::Mesh;
 mod camera;
 pub use self::camera::Camera;
+pub use self::camera::OrthographicCam;
 pub use self::camera::PerspectiveCam;
 mod scene;
 pub use self::scene::Scene;
