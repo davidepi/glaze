ce329fef344bfc73282a315e3e051715163f8266
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-09-25 17:03:06 +0200

    feat: parse textures
    In addition:
    - added Library<T> used to store tuples (u16, String, T), useful for
      Materials and Texture libraries
    - changed the ParsedContent signature. It requires more memory for
      this dumb V1 parser but allows me to load content on-demand from
      the fs in more advanced parsers (coming soon™)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod geometry;
 mod interactive;
+mod materials;
 mod parser;
 mod vulkan;
 pub use interactive::GlazeApp;
---
+++ src/materials/library.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+use std::iter::FromIterator;
+use std::slice::Iter;
+
+#[derive(Debug, Clone)]
+pub struct Library<T> {
+    data: Vec<(u16, String, T)>,
+}
+
+impl<T> Library<T> {
+    pub fn new() -> Library<T> {
+        Library { data: Vec::new() }
+    }
+
+    pub fn with_capacity(capacity: u16) -> Library<T> {
+        Library {
+            data: Vec::with_capacity(capacity as usize),
+        }
+    }
+
+    pub fn insert(&mut self, name: &str, data: T) -> u16 {
+        let id = self.data.len() as u16;
+        self.data.push((id, name.to_string(), data));
+        id
+    }
+
+    pub fn get(&self, id: u16) -> Option<&T> {
+        if id < self.data.len() as u16 {
+            unsafe { Some(&self.data.get_unchecked(id as usize).2) }
+        } else {
+            None
+        }
+    }
+
+    pub fn len(&self) -> u16 {
+        self.data.len() as u16
+    }
+
+    pub fn names(&self) -> HashMap<&str, u16> {
+        self.data
+            .iter()
+            .map(|(id, name, _)| (name.as_str(), *id))
+            .collect()
+    }
+
+    pub fn iter(&self) -> &[(u16, String, T)] {
+        &self.data
+    }
+}
+
+impl<T> IntoIterator for Library<T> {
+    type Item = (u16, String, T);
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
+    }
+}
+
+impl<T> FromIterator<(u16, String, T)> for Library<T> {
+    fn from_iter<I: IntoIterator<Item = (u16, String, T)>>(iter: I) -> Self {
+        let data: Vec<(u16, String, T)> = iter.into_iter().collect();
+        Library { data }
+    }
+}
